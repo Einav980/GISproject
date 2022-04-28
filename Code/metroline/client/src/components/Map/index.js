d480de8f8e622b3dd2b47a79af1129ca09cb383b
einav malcka <einav980@gmail.com>
2022-04-29 02:18:48 +0300

    station indication on map change

--- Code/metroline/client/src/components/Map/index.js
+++ Code/metroline/client/src/components/Map/index.js
@@ -1,5 +1,16 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import center from '../../constants';
+import lines from '../../leafletData/MetroLines/linesData.json'
+import features from "../../leafletData/MetroStations/data.json"
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 const Map = () => {
   return (
@@ -8,11 +19,26 @@ const Map = () => {
       zoom={13}
       scrollWheelZoom={true}
       style={{ width: '100vw', height: '100vh' }}>
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-    </MapContainer>
+
+      {features.features.map(station => (
+        <Marker
+          position={[station.geometry.coordinates[1], station.geometry.coordinates[0]]}>
+        </Marker>
+      ))}
+
+      {/* {lines.features.map(line => (
+        <Polyline>
+          key = {line.properties.DESCRPTION}
+          position = {line.geometry.coordinates}
+        </Polyline>
+      ))} */}
+
+    </MapContainer >
   );
 };
 
